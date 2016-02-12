3fa0d05de0a6a3b84f344ebdd8ccf957fdbc3928
Roman Maccaruni <rommacc@yahoo.it>
2016-02-12 20:39:14 +0000

    add cloud of points and reworked rotations

--- data_faker.cpp
+++ data_faker.cpp
@@ -3,35 +3,99 @@
 #include <iomanip>
 #include <random>
 
+#include "math_lib.h"
+
 using namespace std;
 
-#define SAMPLES           400
+#define SAMPLES           1000
 #define dt                1./400.
 #define T0                0
-#define THETA_DEG         43              // [0, 360] angle between v and x-axis
-#define PHI_DEG           12              // [0, 180] angle between v and z-axis
-#define RADIUS            1/0.48          // [0, inf[
 
-#define NOISE_FRAC        0.1
+// Center of Mass polar coordinates
+#define THETA_DEG         135                  // [0, 360] angle between v and x-axis
+#define PHI_DEG           35                   // [0, 180] angle between v and z-axis
+#define ALPHA_DEG         75                   // [0, 360] rigid body angle
+#define RADIUS            1                    // [0, inf[
 
-#define RAD_TO_DEG        57.2957795131   // 180/pi
+// Cloud parameters
+#define A_X           0.12
+#define A_Y           0.25
+#define A_Z           0.50
+
+// Conversions and constants
+#define RAD_TO_DEG    57.2957795131             // 180/pi
+#define M_PI          3.14159265358979323846    /* pi */
+
+std::default_random_engine generator;
+std::uniform_real_distribution<double> dist_u(0.0,1.0);
+std::uniform_real_distribution<double> dist_theta(0.0,2.0*M_PI);
+std::uniform_real_distribution<double> dist_phi(0.0,M_PI);
+
+void make_ellipsoid_noise(double &nx, double &ny, double &nz){
+	double u, theta, phi;
+	u = dist_u(generator);
+	theta = dist_theta(generator);
+	phi = dist_phi(generator);
+	nx = A_X * u * cos(theta) * sin(phi);
+	ny = A_Y * u * sin(theta) * sin(phi);
+	nz = A_Z * u * cos(phi);
+}
+
+void make_box_noise(double &nx, double &ny, double &nz){
+	double u;
+	u = dist_u(generator);
+	nx = -A_X/2 + A_X * u;
+	u = dist_u(generator);
+	ny = -A_Y/2 + A_Y * u;
+	u = dist_u(generator);
+	nz = -A_Z/2 + A_Z * u;
+}
+
+void make_no_noise(double &nx, double &ny, double &nz){
+	nx = 0.0;
+	ny = 0.0;
+	nz = 0.0;
+}
+
+void rotate(double &x, double &y, double &z){
+	VEC3D vr;
+	MAT3D rot_z1, rot_y, rot_z2;
+	vr = set_vec3d(x, y, z);
+	rot_z1 = make_rotation(set_vec3d(0.0, 0.0, 1.0), ALPHA_DEG/RAD_TO_DEG);
+	rot_y = make_rotation(set_vec3d(0.0, 1.0, 0.0), PHI_DEG/RAD_TO_DEG);
+	rot_z2 = make_rotation(set_vec3d(0.0, 0.0, 1.0), THETA_DEG/RAD_TO_DEG);
+	vr = rotate_vec3d(vr, rot_z1);
+	vr = rotate_vec3d(vr, rot_y);
+	vr = rotate_vec3d(vr, rot_z2);
+	x = vr.x;
+	y = vr.y;
+	z = vr.z;
+}
+
+void (*make_noise)(double &, double &, double &);
 
 int main(){
+	double x0,y0,z0;
+	x0 = 0.0;
+	y0 = 0.0;
+	z0 = RADIUS;
 
-	std::default_random_engine generator;
-	std::normal_distribution<double> distribution(0,NOISE_FRAC*RADIUS);
+//	make_noise = &make_no_noise;
+//	make_noise = &make_box_noise;
+	make_noise = &make_ellipsoid_noise;
 
 	ofstream data("fake_data.txt");
 	for(int i=0; i<SAMPLES; i++){
 		double t,x,y,z;
-		double noise;
+
+		double noise_x, noise_y, noise_z;
+		make_noise(noise_x, noise_y, noise_z);
+		
 		t = T0 + i*dt;
-		noise = distribution(generator);
-		x = RADIUS * cos(THETA_DEG/RAD_TO_DEG) * sin(PHI_DEG/RAD_TO_DEG) + noise;
-		noise = distribution(generator);
-		y = RADIUS * sin(THETA_DEG/RAD_TO_DEG) * sin(PHI_DEG/RAD_TO_DEG) + noise;
-		noise = distribution(generator);
-		z = RADIUS * cos(PHI_DEG/RAD_TO_DEG) + noise;
+		x = x0 + noise_x;
+		y = y0 + noise_y;
+		z = z0 + noise_z;
+		rotate(x, y, z);
 
 		data << fixed << setprecision(3) << t << "\t" << x << "\t" << y << "\t" << z << endl;
 	}
--- makefile
+++ makefile
@@ -1,2 +1,3 @@
 all: 
-	$(CXX) -std=c++11 -o data_faker.exe data_faker.cpp
+	$(CXX) -std=c++11 -o data_faker.exe math_lib.c data_faker.cpp
+	./data_faker.exe
